0ea035743418ec00f7adfd768ee761a754afdb15
GNANAJEBASEELAN_G <gjs242004@gmail.com>
2026-02-11 20:15:55 +0530

    view port updated

--- modelforge/modelforge/settings.py
+++ modelforge/modelforge/settings.py
@@ -124,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",   # if using project-level static
+]
